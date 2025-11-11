7d021667066fd95705400adb2111d130b93b5af1
teja143kt <tejakutchula@gmail.com>
2025-11-11 23:06:12 +0530

    bg adding

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -115,13 +115,13 @@ const LandingPage = () => {
 
       {/* ✅ HOW IT WORKS */}
       <section className="lp-section alt" id="how">
-        <h2>How It Works</h2>
+        <h2 >How It Works</h2>
 
-        <div className="lp-steps">
-          <div className="lp-step"><span>1</span> Create Account</div>
-          <div className="lp-step"><span>2</span> Add Students</div>
-          <div className="lp-step"><span>3</span> Track Attendance & Grades</div>
-          <div className="lp-step"><span>4</span> Share Reports</div>
+        <div className="lp-steps " style={{marginTop:'30px'}}>
+          <div className="lp-step"><img src="https://www.shutterstock.com/image-photo/sign-online-registration-form-register-600nw-2486327787.jpg" alt="Create Account" ></img>Create Account</div>
+          <div className="lp-step"><img src="https://img.freepik.com/free-photo/young-woman-attend-courses-girl-student-studying-holding-notebooks-showing-thumb-up-approval-recommending-company-standing-blue-background_1258-70145.jpg?semt=ais_hybrid&w=740&q=80" alt="Add Students" ></img> Add Students</div>
+          <div className="lp-step"><img src="https://akriviahcm.com/blog/wp-content/uploads/2024/03/Features-of-time-and-attendance-system.jpg" alt="Track Attendance & Grades" ></img> Track Attendance & Grades</div>
+          <div className="lp-step"><img src="https://media.istockphoto.com/id/1456192121/photo/close-up-photo-of-woman-hands-writing-report-on-a-paper-in-the-cafe.jpg?s=612x612&w=0&k=20&c=PympWRSIEXmYzVgC1IStxPty1L5stVplu1HvaqrEuIs=" alt="Share Reports" ></img> Share Reports</div>
         </div>
       </section>
 
@@ -129,8 +129,9 @@ const LandingPage = () => {
       <section className="lp-section" id="roles">
         <h2>Designed for Everyone</h2>
 
-        <div className="lp-grid roles">
+        <div className="lp-grid roles" style={{marginTop:'30px'}}>
           <div className="lp-card role">
+            <img  src="https://www.shutterstock.com/image-photo/business-team-meeting-600nw-46954597.jpg" alt="img"></img>
             <h3>Administrators</h3>
             <ul>
               <li>Manage school data</li>
@@ -140,6 +141,7 @@ const LandingPage = () => {
           </div>
 
           <div className="lp-card role">
+            <img  src="https://media.istockphoto.com/id/1481370752/photo/two-colleagues-working-on-laptop-in-office-female-hispanic-help-desk-coordinator-collaborates.jpg?s=612x612&w=0&k=20&c=Ri3QjPVe8isWW48uwHOnk6pp7x66LIBYNSjJwjpUEwk=" alt="img"></img>
             <h3>Coordinators</h3>
             <ul>
               <li>Manage sections</li>
@@ -149,6 +151,7 @@ const LandingPage = () => {
           </div>
 
           <div className="lp-card role">
+            <img  src="https://media.istockphoto.com/id/1430133631/photo/young-teacher-helping-teenager-students-at-college-learning-technology-and-science-in.jpg?s=612x612&w=0&k=20&c=6l6irCGKHe9ggxMeD5QbS4fvIdsTLsmg8HZbpMsYFVE=" alt="img"></img>
             <h3>Teachers</h3>
             <ul>
               <li>Mark attendance</li>
@@ -158,6 +161,7 @@ const LandingPage = () => {
           </div>
 
           <div className="lp-card role">
+            <img  src="https://st4.depositphotos.com/12985790/21900/i/450/depositphotos_219003960-stock-photo-portrait-focused-woman-headphones-taking.jpg" alt="img"></img>
             <h3>Students</h3>
             <ul>
               <li>Attendance %</li>
